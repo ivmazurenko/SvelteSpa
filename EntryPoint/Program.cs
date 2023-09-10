11dcab75c764c079766ab8bdc18239c5a1b88272
imazurenko <ivmazurenko@yandex.ru>
2023-09-10 14:01:44 +0300

    temp

--- EntryPoint/Program.cs
+++ EntryPoint/Program.cs
@@ -1,7 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
-
+app.MapControllers();
 app.Run();
